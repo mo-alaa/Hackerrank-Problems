03a823aef7e40d654f5e7517ce5aab9c1faeccb5
Mohamed Alaa <41386480+mo-alaa@users.noreply.github.com>
2018-08-31 19:55:23 +0200

    Create Revising the Select Query II.sql
    Hackerrank url for the problem: https://www.hackerrank.com/challenges/revising-the-select-query-2/problem

---
+++ SQL/Basic Select/Easy/Revising the Select Query II.sql
@@ -0,0 +1,8 @@
+/*
+Language:   T-SQL 
+Author:     Mohamed Alaa
+Hackerrank: https://www.hackerrank.com/mo_alaa
+Github:     https://github.com/mo-alaa
+*/
+
+SELECT NAME FROM CITY WHERE COUNTRYCODE='USA' AND POPULATION>120000;
