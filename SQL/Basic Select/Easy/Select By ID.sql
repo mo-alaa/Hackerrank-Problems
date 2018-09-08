3e3efc742e0bd0fec8550113baab94ad82f22784
Mohamed Alaa <41386480+mo-alaa@users.noreply.github.com>
2018-09-08 19:18:42 +0200

    Update Select By ID.sql

--- SQL/Basic Select/Easy/Select By ID.sql
+++ SQL/Basic Select/Easy/Select By ID.sql
@@ -3,7 +3,7 @@ Language:   T-SQL
 Author:     Mohamed Alaa
 Github:     https://github.com/mo-alaa
 Hackerrank: https://www.hackerrank.com/mo_alaa
-Problem: https://www.hackerrank.com/challenges/select-by-id/submissions/code/80813554
+Problem: https://www.hackerrank.com/challenges/select-by-id/problem
 */
 
 SELECT * FROM CITY WHERE ID=1661;
