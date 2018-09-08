aa425efb4029c76126876cdeaca10a4c8c60d3c1
Mohamed Alaa <41386480+mo-alaa@users.noreply.github.com>
2018-09-08 19:25:12 +0200

    Update Select All.sql

--- SQL/Basic Select/Easy/Select All.sql
+++ SQL/Basic Select/Easy/Select All.sql
@@ -1,8 +1,8 @@
 /*
 Language:   T-SQL 
 Author:     Mohamed Alaa
-Hackerrank: https://www.hackerrank.com/mo_alaa
 Github:     https://github.com/mo-alaa
+Hackerrank: https://www.hackerrank.com/mo_alaa
+Problem:    https://www.hackerrank.com/challenges/select-all-sql 
 */
-
 SELECT * FROM CITY;
