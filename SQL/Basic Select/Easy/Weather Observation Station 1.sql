5b4c4dbec4b469d3e210413f32df50ce5bb4c2d8
Mohamed Alaa <41386480+mo-alaa@users.noreply.github.com>
2018-09-08 19:28:27 +0200

    Create Weather Observation Station 1.sql

---
+++ SQL/Basic Select/Easy/Weather Observation Station 1.sql
@@ -0,0 +1,9 @@
+/*
+Language:   T-SQL 
+Author:     Mohamed Alaa
+Github:     https://github.com/mo-alaa
+Hackerrank: https://www.hackerrank.com/mo_alaa
+Problem:    https://www.hackerrank.com/challenges/weather-observation-station-1/problem
+*/
+
+SELECT CITY, STATE FROM STATION;
