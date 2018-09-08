16745148bf369f9ae9f350f52fd7e8fa81b2b8ff
Mohamed Alaa <41386480+mo-alaa@users.noreply.github.com>
2018-09-08 19:22:42 +0200

    Update Revising the Select Query I.sql

--- SQL/Basic Select/Easy/Revising the Select Query I.sql
+++ SQL/Basic Select/Easy/Revising the Select Query I.sql
@@ -1,1 +1,9 @@
+/*
+Language:   T-SQL 
+Author:     Mohamed Alaa
+Github:     https://github.com/mo-alaa
+Hackerrank: https://www.hackerrank.com/mo_alaa
+Problem:    https://www.hackerrank.com/challenges/revising-the-select-query/problem  
+*/
+
 SELECT * FROM CITY WHERE COUNTRYCODE='USA' AND POPULATION>100000;
