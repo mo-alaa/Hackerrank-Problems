cc584ceff88ec10aca3444491c4f9c141176b597
Mohamed Alaa <41386480+mo-alaa@users.noreply.github.com>
2018-09-08 19:18:05 +0200

    Create Japanese Cities' Names.sql

---
+++ SQL/Basic Select/Easy/Japanese Cities' Names.sql
@@ -0,0 +1,9 @@
+/*
+Language:   T-SQL 
+Author:     Mohamed Alaa
+Github:     https://github.com/mo-alaa
+Hackerrank: https://www.hackerrank.com/mo_alaa
+Problem: https://www.hackerrank.com/challenges/japanese-cities-name/problem
+*/
+
+SELECT NAME FROM CITY WHERE COUNTRYCODE='JPN'
