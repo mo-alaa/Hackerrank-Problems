259549661d68cdd469d3448c6b9d8c5c5af426e0
Mohamed Alaa <41386480+mo-alaa@users.noreply.github.com>
2018-09-08 19:21:29 +0200

    Update Japanese Cities' Attributes.sql

--- SQL/Basic Select/Easy/Japanese Cities' Attributes.sql
+++ SQL/Basic Select/Easy/Japanese Cities' Attributes.sql
@@ -3,7 +3,7 @@ Language:   T-SQL
 Author:     Mohamed Alaa
 Github:     https://github.com/mo-alaa
 Hackerrank: https://www.hackerrank.com/mo_alaa
-Problem: https://www.hackerrank.com/challenges/japanese-cities-attributes/submissions/code/80813622
+Problem: https://www.hackerrank.com/challenges/japanese-cities-attributes/problem
 */
 
 SELECT * FROM CITY WHERE COUNTRYCODE='JPN';
