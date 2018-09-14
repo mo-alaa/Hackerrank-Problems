09329f6b72a0501a82d4f348644aebfdd9aeacf8
Mohamed Alaa <41386480+mo-alaa@users.noreply.github.com>
2018-09-14 21:37:29 +0200

    Create Employee Names.sql

---
+++ SQL/Basic Select/Easy/Employee Names.sql
@@ -0,0 +1,10 @@
+/*
+Language:   T-SQL 
+Author:     Mohamed Alaa
+Github:     https://github.com/mo-alaa
+Hackerrank: https://www.hackerrank.com/mo_alaa
+Problem:    https://www.hackerrank.com/challenges/name-of-employees/problem
+*/
+
+
+SELECT NAME FROM EMPLOYEE ORDER BY NAME ASC;
