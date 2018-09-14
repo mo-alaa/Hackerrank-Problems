26b60ce9a8084db557d5d33971cc8f95a2eac3ef
Mohamed Alaa <41386480+mo-alaa@users.noreply.github.com>
2018-09-14 21:03:28 +0200

    Create Weather Observation Station 6.sql

---
+++ SQL/Basic Select/Easy/Weather Observation Station 6.sql
@@ -0,0 +1,28 @@
+/*
+Language:   T-SQL 
+Author:     Mohamed Alaa
+Github:     https://github.com/mo-alaa
+Hackerrank: https://www.hackerrank.com/mo_alaa
+Problem:    https://www.hackerrank.com/challenges/weather-observation-station-6/problem
+*/
+
+
+SELECT DISTINCT CITY FROM STATION WHERE CITY like '[AEIOU]%';
+
+
+
+
+
+
+
+/*
+Explanation:
+------------
+The LIKE operator is used in a WHERE clause to search for a specified pattern in a column.
+% - The percent sign represents zero, one, or multiple characters.
+% - The percent sign is a wildcard character. A wildcard character is used to substitute any other character(s) in a string.
+For more information:  https://www.w3schools.com/sql/sql_like.asp  https://www.w3schools.com/sql/sql_wildcards.asp
+
+Another solution without using regular expressions can be: 
+SELECT DISTINCT CITY FROM STATION WHERE CITY LIKE 'A%' OR CITY LIKE 'I%' OR CITY LIKE 'E%' OR CITY LIKE'O%' OR CITY LIKE 'U%';
+*/
