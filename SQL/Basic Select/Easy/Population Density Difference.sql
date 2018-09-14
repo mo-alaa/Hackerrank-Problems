145c3606df4e02056f767f2be570af0897a21c46
Mohamed Alaa <41386480+mo-alaa@users.noreply.github.com>
2018-09-14 21:41:21 +0200

    Create Population Density Difference.sql

---
+++ SQL/Basic Select/Easy/Population Density Difference.sql
@@ -0,0 +1,10 @@
+/*
+Language:   T-SQL 
+Author:     Mohamed Alaa
+Github:     https://github.com/mo-alaa
+Hackerrank: https://www.hackerrank.com/mo_alaa
+Problem:    https://www.hackerrank.com/challenges/population-density-difference/problem
+*/
+
+
+SELECT (MAX(POPULATION) - MIN(POPULATION)) FROM CITY;
