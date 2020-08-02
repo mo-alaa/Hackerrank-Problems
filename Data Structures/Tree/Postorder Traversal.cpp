d3f0497c964ae96addc9a363a6d9c0525a0b473d
Mohamed Alaa <41386480+mo-alaa@users.noreply.github.com>
2020-08-02 14:25:43 +0200

    Update Postorder Traversal.cpp

--- Data Structures/Tree/Postorder Traversal.cpp
+++ Data Structures/Tree/Postorder Traversal.cpp
@@ -3,7 +3,7 @@ Language:   C++
 Author:     Mohamed Alaa
 Github:     https://github.com/mo-alaa
 Hackerrank: https://www.hackerrank.com/mo_alaa
-Problem:   https://www.hackerrank.com/challenges/tree-postorder-traversal/problem
+Problem:    https://www.hackerrank.com/challenges/tree-postorder-traversal/problem
 */ 
 
 void postOrder(Node *root) {
